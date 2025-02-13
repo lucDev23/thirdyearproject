e8344eddbb3819c6f95647df5b62c91773de0237
Lucas Gonzalez <lgonzalez@urudatasoftware.com>
2025-02-13 13:08:35 -0300

    feat: fix files indentation

--- public/js/game.js
+++ public/js/game.js
@@ -11,16 +11,16 @@ import { joinGame, sendBismarckHitSwordfish, sendSwordfishHitBismarck, setupBism
 const parentDiv = document.getElementById("phaser-game");
 
 const config = {
-    type: Phaser.AUTO,
-    width: parentDiv.clientWidth,
-    height: parentDiv.clientHeight,
-    parent: "phaser-game",
-    transparent: true,
-    physics: {
-        default: "arcade",
-        arcade: { debug: false }
-    },
-    scene: { preload, create, update }
+	type: Phaser.AUTO,
+	width: parentDiv.clientWidth,
+	height: parentDiv.clientHeight,
+	parent: "phaser-game",
+	transparent: true,
+	physics: {
+		default: "arcade",
+		arcade: { debug: false }
+	},
+	scene: { preload, create, update }
 };
 
 const game = new Phaser.Game(config);
@@ -30,41 +30,41 @@ let airship = null;
 let playerRole = null;
 
 function preload() {
-    this.load.image("bismarck", "/assets/bismarck.png");
-    this.load.image("bismarck_missile", "/assets/bismarck_missile.png");
-    this.load.image("airship", "/assets/airship.png");
-    this.load.image("airship_missile", "/assets/airship_missile.png");
+	this.load.image("bismarck", "/assets/bismarck.png");
+	this.load.image("bismarck_missile", "/assets/bismarck_missile.png");
+	this.load.image("airship", "/assets/airship.png");
+	this.load.image("airship_missile", "/assets/airship_missile.png");
 }
 
 async function create() {
-    setupTerrain(this);
+	setupTerrain(this);
 
 	this.bismarckMissiles = this.physics.add.group();
-    this.swordfishMissiles = this.physics.add.group();
+	this.swordfishMissiles = this.physics.add.group();
 
-    playerRole = await joinGame();
+	playerRole = await joinGame();
 
-    socket.on("create-bismarck", (position) => {
-        if (!bismarck) {
-            console.log("Creando Bismarck...");
-            bismarck = new Bismarck(this, position.x, position.y, socket);
-            setupBismarckSocketListeners(bismarck, this);
-        }
+	socket.on("create-bismarck", (position) => {
+		if (!bismarck) {
+			console.log("Creando Bismarck...");
+			bismarck = new Bismarck(this, position.x, position.y, socket);
+			setupBismarckSocketListeners(bismarck, this);
+		}
 
-        this.physics.add.overlap(bismarck, this.swordfishMissiles, (bismarck, missile) => {
+		this.physics.add.overlap(bismarck, this.swordfishMissiles, (bismarck, missile) => {
 			console.log("¡Colisión detectada!");
 			missile.destroy();
 
 			sendSwordfishHitBismarck();
 		});
-    });
+	});
 
-    socket.on("create-swordfish", (position) => {
-        if (!airship) {
-            console.log("Creando Swordfish...");
-            airship = new Swordfish(this, position.x, position.y, socket);
-            setupSwordfishSocketListeners(airship, this);
-        }
+	socket.on("create-swordfish", (position) => {
+		if (!airship) {
+			console.log("Creando Swordfish...");
+			airship = new Swordfish(this, position.x, position.y, socket);
+			setupSwordfishSocketListeners(airship, this);
+		}
 
 		this.physics.add.overlap(airship, this.bismarckMissiles, (airship, missile) => {
 			console.log("¡Colisión detectada!");
@@ -72,25 +72,25 @@ async function create() {
 
 			sendBismarckHitSwordfish();
 		});
-    });
+	});
 
-    // **Eliminar el Bismarck cuando el servidor lo indique**
-    socket.on("remove-bismarck", () => {
-        if (bismarck) {
-            // console.log("Eliminando Bismarck...");
-            bismarck.destroy();
-            bismarck = null;
-        }
-    });
+	// **Eliminar el Bismarck cuando el servidor lo indique**
+	socket.on("remove-bismarck", () => {
+		if (bismarck) {
+			// console.log("Eliminando Bismarck...");
+			bismarck.destroy();
+			bismarck = null;
+		}
+	});
 
-    // **Eliminar el Swordfish cuando el servidor lo indique**
-    socket.on("remove-swordfish", () => {
-        if (airship) {
-            // console.log("Eliminando Swordfish...");
-            airship.destroy();
-            airship = null;
-        }
-    });
+	// **Eliminar el Swordfish cuando el servidor lo indique**
+	socket.on("remove-swordfish", () => {
+		if (airship) {
+			// console.log("Eliminando Swordfish...");
+			airship.destroy();
+			airship = null;
+		}
+	});
 
 	socket.on("bismarck-winner", () => {
 		const popup = document.getElementById("popup");
@@ -104,11 +104,11 @@ async function create() {
 }
 
 function update() {
-    if (bismarck && playerRole === "bismarck") {
-        bismarck.update();
-    }
+	if (bismarck && playerRole === "bismarck") {
+		bismarck.update();
+	}
 
-    if (airship && playerRole === "swordfish") {
-        airship.update();
-    }
+	if (airship && playerRole === "swordfish") {
+		airship.update();
+	}
 }
